682ddcaa9f6fba7fca4b4f66fa3595dc60d3bb9d
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-07-08 06:40:03 -0300

    Refactor de la conexión con clases

--- database/MySQLi/Connection.php
+++ database/MySQLi/Connection.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\MySQLi;
+
 require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
@@ -7,18 +9,49 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable("C:\Users\lauta\Documents\dev\platzi-php-database");
 $dotenv->load();
 
-$server = $_ENV["server"];
-$database = $_ENV["database"];
-$username = $_ENV["username"];
-$password = $_ENV["password"];
-$port = $_ENV["port"];
+// Patrón singleton
+class Connection {
+    private static $instance;
+    private $connection;
+
+    public static function getInstance(){
+        if (!self::$instance instanceof self)
+            self::$instance = new self;
+
+        return self::$instance;
+    }
+
+    public function getConnection(){
+        return $this->connection;
+    }
+
+    private function __construct(){
+        $this->make_connection();
+    }
+
+    private function make_connection(){
+        $server = $_ENV["server"];
+        $database = $_ENV["database"];
+        $username = $_ENV["username"];
+        $password = $_ENV["password"];
+        $port = $_ENV["port"];
+
+        $mysqli = new \mysqli($server, $username, $password, $database, $port);
+
+        if ($mysqli->connect_errno)
+            die("Falló la conexión: {$mysqli->connect_error}");
+
+        $this->setUTF8($mysqli);
+
+        $this->connection = $mysqli;
+    }
+
+    private function setUTF8($connection){
+        $setname = $connection->prepare("SET NAMES 'utf8'");
+        $setname->execute();
+    }
+}
 
 
-$mysqli = new mysqli($server, $username, $password, $database, $port);
 
-if ($mysqli->connect_errno)
-    die("Falló la conexión: {$mysqli->connect_error}");
 
-$setname = $mysqli->prepare("SET NAMES 'utf8'");
-
-$setname->execute();
--- database/PDO/Connection.php
+++ database/PDO/Connection.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Database\PDO;
+
 require 'vendor/autoload.php';
 
 use Dotenv\Dotenv;
@@ -7,18 +9,42 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable("C:\Users\lauta\Documents\dev\platzi-php-database");
 $dotenv->load();
 
-$server = $_ENV["server"];
-$database = $_ENV["database"];
-$username = $_ENV["username"];
-$password = $_ENV["password"];
-$port = $_ENV["port"];
+class Connection {
+    private static $intance;
+    private $connection;
 
+    public static function getIntance(){
+        if (!self::$intance instanceof self)
+            self::$intance = new self;
 
-try {
-    $connection = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+        return self::$intance;
+    }
 
-    $setnames = $connection->prepare("SET NAMES 'utf8'");
-    $setnames->execute();
-} catch (PDOException $e) {
-    die("Falló la conexión: {$e->getMessage()}");
+    public function getConnection(){
+        return $this->connection;
+    }
+
+    private function __construct(){
+        $this->makeConnection();
+    }
+
+    private function makeConnection(){
+        $server = $_ENV["server"];
+        $database = $_ENV["database"];
+        $username = $_ENV["username"];
+        $password = $_ENV["password"];
+
+        try {
+            $connection = new \PDO("mysql:host=$server;dbname=$database", $username, $password);
+            $this->setUTF8($connection);
+            $this->connection = $connection;
+        } catch (\PDOException $e) {
+            die("Falló la conexión: {$e->getMessage()}");
+        }
+    }
+
+    private function setUTF8($connection){
+        $setnames = $connection->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+    }
 }
