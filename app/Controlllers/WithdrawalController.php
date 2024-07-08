b3b045d19012a1249f381a144ea887e2760cb095
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-07-08 07:09:30 -0300

    Creación de controladores para la aplicación

---
+++ app/Controlllers/WithdrawalController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers;
+
+class WithdrawalController {
+    /**
+     * Muestra una lista de recursos
+     */
+    public function index(){}
+
+    /**
+     * Muestra un formulario para crear un nuevo recurso
+     */
+    public function create(){}
+
+    /**
+     * Guarda un nuevo recurso en la base de datos
+     */
+    public function store(){}
+
+    /**
+     * Muestra un unico recurso especificado
+     */
+    public function show(){}
+
+    /**
+     * Muestra un formulario para editar un recursp
+     */
+    public function edit(){}
+
+    /**
+     * Actualiza un recurso especifico en la base de datos
+     */
+    public function update(){}
+
+    /**
+     * Elimina un recurso de la base de datos
+     */
+    public function destroy(){}
+}
